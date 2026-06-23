2e083e5d2af91b45fc2c079dc06d09fa892718c7
Jeni Wang <tmps880305@gmail.com>
2026-06-23 10:12:24 +0800

    feat: Change app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.jywang.mahjongscoreboard',
-  appName: '麻雀スコアボード',
+  appName: '麻雀スコア',
   webDir: 'dist'
 };
 
